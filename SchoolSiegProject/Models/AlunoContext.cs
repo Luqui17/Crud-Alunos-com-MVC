96661a467f3304eef782362edf02877d8e4162cc
Luqui17 <lucasgosilva17@gmail.com>
2023-02-24 15:03:07 -0300

    Projeto finalizado

---
+++ SchoolSiegProject/Models/AlunoContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SchoolSiegProject.Models
+{
+    public class AlunoContext : DbContext
+    {
+        public DbSet<Aluno> Alunos { get; set; }
+    }
+}
